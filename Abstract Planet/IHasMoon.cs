afc506a56b6acde4fa914f43a5f93c486e906198
jaswa <jaswa@JASWAL>
2017-07-28 12:46:06 -0400

    created IHasMoon interface

---
+++ Abstract Planet/IHasMoon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * name: Parth Jaswal
+ * date: july 28, 2017
+ * description: this is a interface that defines a MoonCount property that must be implemented in any class sthat sucbscribe to it
+ * version: 0.1 - created interface IHasMoon
+ */
+
+namespace Abstract_Planet
+{
+    /// <summary>
+    /// this is IHasMoon interface
+    /// </summary>
+    interface IHasMoon
+    {
+        //public property
+        bool HasMoon();
+    }
+}
